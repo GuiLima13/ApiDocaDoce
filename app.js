b6f89936e56a411a6062988b4741ee8e09122b35
Guilherme Lima <guilherme.lima13@yahoo.com.br>
2020-07-01 03:02:57 -0300

    mais uma tentantiva de configurar o cors

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 app.use((req,res,next)=>{
     
     res.header("Access-Control-Allow-Origin","*")
+    res.header("Access-Control-Allow-Headers","*")
     app.use(cors())
     next();
 })
