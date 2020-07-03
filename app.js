195e0147714854026f04e06dcabae55df03e78cd
GuiLima13 <guilherme.lima13@yahoo.com.br>
2020-07-02 22:45:25 -0300

    testando cors

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ var indexRouter = require('./routes/index');
 var usuarioRouter = require('./routes/usuarioRouter');
 
 var app = express();
-
+/*
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    app.use(cors())
+    
  next()
 }
-)
-
+)*/
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
